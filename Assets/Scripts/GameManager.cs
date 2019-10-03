9023877ebbbb820312d46477464b27d8454cd0f7
Austin <austinkobayashi@gmail.com>
2019-10-02 19:55:25 -0700

    Added pillar prefab. Fixed gamemanager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,13 +7,13 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour {
 
     //Variable for the pillar prefab (Set in editor)
-    public GameObject pillar;
+    public GameObject Pillar;
     
     //Variable for the fade gameobject (set in editor)
     public GameObject fade;
     
-    public float MaxY = 3f;
-    public float MinY = -3f;
+    public float MaxSpawnY = 3f;
+    public float MinSpawnY = -3f;
     public float PillarSpeed = 3;
     
     //Text that shows your score
@@ -35,20 +35,40 @@ public class GameManager : MonoBehaviour {
     //Variable to track whether the game is still being played or if the player lost.
     private bool _gameOver = false;
     
-    void Update() {
-    }
-
-
-    void SpawnPillar() {
-
-    }
     
+    
+    // Update is called once per frame
+    void Update() {
+        _time += Time.deltaTime;
+        if (_time >= _spawnTime) {
+            SpawnPillar();
+            _time = 0f;
+        }
+	
+        if(_gameOver && Input.anyKeyDown)
+            Restart();
+
+    }
+
+
+
+    
+    void SpawnPillar() {
+        float y = Random.Range(MinSpawnY, MaxSpawnY);
+        Vector2 pos = new Vector2(13, y);
+        GameObject newPillar = Instantiate(Pillar, pos,  Quaternion.identity) as GameObject;
+        newPillar.GetComponent<Wall>().SetSpeed(PillarSpeed);
+    }
+
     
     
     
     public void IncreaseScore() {
+        _score++;
+        ScoreText.text = _score.ToString();
     }
 
+    
     
 
     public void GameOver() {
---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour {
+
+    private Rigidbody2D _rigid;
+    
+    void Awake() {
+        _rigid = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        if (transform.position.x <= -15f)
+            Destroy(gameObject);
+    }
+
+
+    public void SetSpeed(float speed) {
+        _rigid.velocity = new Vector2(-1f * speed, 0f);
+    }
+}
